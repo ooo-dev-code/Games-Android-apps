7dc2c10dbd21dff8b4c8ab7a5db5ec8f955faf79
ooo <183236447+ooo-dev-code@users.noreply.github.com>
2025-01-11 16:27:59 +0100

    Create main.java

---
+++ Java/Space_Ships/main.java
@@ -0,0 +1,27 @@
+package SpaceShip;
+import java.awt.Dimension;
+import javax.swing.JFrame;
+
+public class main {
+    
+     public static void main(String[] args) {
+
+        int tileSize = 32; 
+        int rows = 16;
+        int columns = 16;
+        int boardHeight = columns*tileSize;
+        int boardWidth = rows*tileSize;
+        JFrame frame = new JFrame();
+        frame.setTitle("SpaceShip");
+        frame.setResizable(true);
+        frame.setPreferredSize(new Dimension(boardWidth, boardHeight));
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        SpaceInvaders space_shipers = new SpaceInvaders();
+        frame.add(space_shipers);
+        frame.pack();
+        space_shipers.requestFocus();
+        frame.setVisible(true);
+    }
+
+}
