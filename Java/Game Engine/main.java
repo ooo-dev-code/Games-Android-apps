abae923332da869e4854c3f261c200ad3d34ca0b
ooo <183236447+ooo-dev-code@users.noreply.github.com>
2025-01-12 14:10:37 +0100

    Update main.java

--- Java/Game Engine/main.java
+++ Java/Game Engine/main.java
@@ -1,1 +1,31 @@
 
+package Game Engine;
+
+import java.awt.Dimension;
+import javax.swing.JFrame;
+
+public class main {
+    
+     public static void main(String[] args) {
+
+        int tileSize = 32; 
+        int rows = 44;
+        int columns = 20;
+        int boardHeight = columns*tileSize;
+        int boardWidth = rows*tileSize;
+
+        JFrame frame = new JFrame();
+        frame.setTitle("Games");
+        frame.setResizable(true);
+        frame.setPreferredSize(new Dimension(boardWidth, boardHeight));
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        GameEngine engine = new GameEngine();
+        frame.add(engine);
+        frame.pack();
+        engine.requestFocus();
+        
+        frame.setVisible(true);
+    }
+
+}
