a01efb10c84c56814f478228b6a617790714c2b7
ooo <183236447+ooo-dev-code@users.noreply.github.com>
2025-01-16 13:10:08 +0100

    Update Game.java

--- Java/Platformer/LittleDude/Game.java
+++ Java/Platformer/LittleDude/Game.java
@@ -2,6 +2,7 @@ package kirby;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.Rectangle;
@@ -39,14 +40,16 @@ public class Game extends JPanel implements ActionListener, KeyListener{
         int x, y, width, height;
         Image img;
         boolean player;
+        int idImg;
 
-        Block(Image img, int x, int y, int width, int height, boolean player) {
+        Block(Image img, int x, int y, int width, int height, boolean player, int idImg) {
             this.img = img;
             this.x = x;
             this.y = y;
             this.width = width;
             this.height = height;
             this.player = player;
+            this.idImg = idImg;
         }
     }
     class Tile {
@@ -70,30 +73,35 @@ public class Game extends JPanel implements ActionListener, KeyListener{
     }
 
     Block player;
-    Image player_img = new ImageIcon(getClass().getResource("../SpaceShip/assets/ship.png")).getImage();
+    Image player_img = new ImageIcon(getClass().getResource("./assets/Player/playerV1img1.png")).getImage();
     int playerVelocityX = 0;
     int playerVelocityY = 0;
     boolean isStanding = true;
+    int moving = 1;
 
     Image chosen = new ImageIcon(getClass().getResource("../GameEngines/assets/Ground/Blue.png")).getImage();
     Image chosen2 = new ImageIcon(getClass().getResource("../GameEngines/assets/Ground/Brown.png")).getImage();
     Image chosen3 = new ImageIcon(getClass().getResource("../GameEngines/assets/Ground/Pink.png")).getImage();
     Image chosen4 = new ImageIcon(getClass().getResource("../GameEngines/assets/Ground/Green.png")).getImage();
     Image chosen5 = new ImageIcon(getClass().getResource("../GameEngines/assets/Ground/Gray.png")).getImage();
+    Image chosen6 = new ImageIcon(getClass().getResource("./assets/Player/playerV1img1.png")).getImage();
     int idImg = 1;
 
     ArrayList<Tile> tiles = new ArrayList<Tile>();
 
     int gra=1;
+    Timer gameLoop;
+    boolean win = false;
+    boolean gameOver = false;
 
-    public Game() {
+    public Game(String level, Image bg) {
 
         setBackground(Color.BLACK);
         setPreferredSize(new Dimension(boardWidth, boardHeight));
         setFocusable(true);
         addKeyListener(this);
 
-        try (BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\mohamedkaouachi4\\Desktop\\jgame\\kirby\\tile_data5.csv"))) {
+        try (BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\mohamedkaouachi4\\Desktop\\jgame\\kirby\\tile_data" + level + ".csv"))) {
             String line;
             int d = 39;
             int g = 18;
@@ -118,6 +126,9 @@ public class Game extends JPanel implements ActionListener, KeyListener{
                         } 
                         if (value == 5 ) {
                             tiles.add(new Tile(p * tileSize, tiles.size() / rows * tileSize+tileSize*b, tileSize, tileSize, chosen5, idImg, "platform"));
+                        } 
+                        if (value == 6 ) {
+                            tiles.add(new Tile(p * tileSize, tiles.size() / rows * tileSize+tileSize*b, tileSize, tileSize, chosen6, idImg, "escape"));
                         } 
                         b += 1;
                         if (i == d) {
@@ -144,18 +155,32 @@ public class Game extends JPanel implements ActionListener, KeyListener{
             e.printStackTrace();
         }
 
-        player = new Block(player_img, 0, 500, (int)(tileSize*1.5), (int)(tileSize*1.5), true);
-        Timer gameLoop = new Timer(500/60, this); //1000/60 = 16.6
+        player = new Block(player_img, 0, 500, (int)(tileSize*1.5), (int)(tileSize*1.5), true, 1);
+        gameLoop = new Timer(750/60, this); //1000/60 = 16.6
         gameLoop.start();
     }
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
+        g.drawImage(chosen, 0, 0, boardWidth, boardHeight, null);
         for (Tile tile : tiles) {
             g.drawImage(tile.img, tile.x, tile.y, tile.x2, tile.y2, null);
         }
-        g.drawImage(player.img, player.x, player.y, player.width, player.height, null);
+        if (moving == -1) {
+            g.drawImage(player.img, player.x+player.width, player.y, moving*player.width, player.height, null);
+
+        } else {
+            g.drawImage(player.img, player.x, player.y, moving*player.width, player.height, null);
+        }
+
+        if (win) {
+            g.setFont(new Font("Arial", Font.PLAIN, 38));
+            g.setColor(Color.YELLOW);
+            g.drawString("You WON : ", 0, boardHeight/2);
+            g.setFont(new Font("Arial", Font.PLAIN, 19));
+            g.drawString("Press CONTROL to go to the next level ", 0, boardHeight/2+46);
+        }
 
     }
 
@@ -163,17 +188,18 @@ public class Game extends JPanel implements ActionListener, KeyListener{
     public void keyPressed(KeyEvent e) {
         int key = e.getKeyCode();
 
-        if (key == KeyEvent.VK_SPACE) {
-            if (isStanding) {
-                playerVelocityY += -15;
-            }
+        if (key == KeyEvent.VK_SPACE && isStanding) {
+            isStanding = false;
+            playerVelocityY += -15; 
         }
         if (key == KeyEvent.VK_LEFT) {
             playerVelocityX = -5;
+            moving = -1;
         }
 
         if (key == KeyEvent.VK_RIGHT) {
             playerVelocityX = 5;
+            moving = 1;
         }
     }
     @Override
@@ -199,18 +225,23 @@ public class Game extends JPanel implements ActionListener, KeyListener{
     @Override
     public void actionPerformed(ActionEvent e) {
         gra = 1;
+        isStanding = false;
         player.x += playerVelocityX;
         player.y += playerVelocityY;
         for (Tile tile : tiles) {
             if (tile.type == "platform") {
-                if (detectCollision(player, tile)) {
+                if (detectCollision(player, tile) || player.x < 0) {
                     player.y -= playerVelocityY;
                     player.x -= playerVelocityX;
                     isStanding = true;
                     gra = 0;
                     break;
-                } else {
-                    isStanding = false;
+                }
+            }
+            if (tile.type == "escape") {
+                if (detectCollision(player, tile) || player.x < 0) {
+                    win = true;
+                    gameLoop.stop();
                 }
             }
         }
