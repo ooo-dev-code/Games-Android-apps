bdfef9b18ea1530b5d31189b54733841e7230edc
ooo <183236447+ooo-dev-code@users.noreply.github.com>
2025-02-06 15:12:23 +0100

    Update main.java

--- Java/Zelda/main.java
+++ Java/Zelda/main.java
@@ -1,1 +1,31 @@
 
+package Zelda;
+
+import java.awt.Dimension;
+import javax.swing.JFrame;
+
+public class main {
+    
+    public static void main(String[] args) {
+
+        int tileSize = 32; 
+        int rows = 44;
+        int columns = 20;
+        int boardHeight = columns*tileSize;
+        int boardWidth = rows*tileSize;
+
+        JFrame frame = new JFrame();
+        frame.setTitle("Games");
+        frame.setResizable(false);
+        frame.setPreferredSize(new Dimension(boardWidth, boardHeight));
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        Game game = new Game();
+        frame.add(game);
+        frame.pack();
+        game.requestFocus();
+        
+        frame.setVisible(true);
+    }
+
+}
